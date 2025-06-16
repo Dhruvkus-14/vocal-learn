0a88b4ab9d23543ca6dc352cb7093e2888586334
Dhruvkus-14 <dhruvkushwaha484@gmail.com>
2025-06-17 02:18:37 +0530

    Initial commit - VocalLearn clean version

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://40f5cc3fb437ab3e04e2cb0e99eba203@o4509506913370112.ingest.de.sentry.io/4509508896292944",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
